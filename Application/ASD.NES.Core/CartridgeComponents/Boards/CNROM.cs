3fc5d09b3b3bf6cddaa3057122ee403a80cb961e
Stanislav Kuzmich [Art.Stea1th] <art.stea1th@gmail.com>
2018-02-21 13:57:25 +0300

    #7 quick impl. CNROM, M003

--- Application/ASD.NES.Core/CartridgeComponents/Boards/CNROM.cs
+++ Application/ASD.NES.Core/CartridgeComponents/Boards/CNROM.cs
@@ -1,24 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ASD.NES.Core.CartridgeComponents.Boards {
 
     //https://wiki.nesdev.com/w/index.php/CNROM
-    internal abstract class CNROM : Board {
+    internal abstract class CNROM : Board { // TODO: Verify
+
+        private int bank = 0;
 
         protected override byte Read(int address) {
-            throw new NotImplementedException();
+
+            if (address < 0x2000) {
+                return chr[bank][address];
+            }
+            if (address < 0xC000) {
+                if (prg.Count > 1) {
+                    return prg[1][address - 0x8000];
+                }
+                return prg[0][address - 0x8000];
+            }
+            if (address < 0x10000) {
+                return prg[0][address - 0xC000];
+            }
+
+            throw new IndexOutOfRangeException();
         }
 
         protected override void Write(int address, byte value) {
-            throw new NotImplementedException();
-        }
-
-        public override void SetCHR(IReadOnlyList<byte[]> chr) {
-            throw new NotImplementedException();
-        }
-        public override void SetPRG(IReadOnlyList<byte[]> prg) {
-            throw new NotImplementedException();
+            if (address >= 0x8000 && address <= 0xFFFF) {
+                if (address >= 0xFFFA) {
+                    prg[0][address - 0xC000] = value;
+                }
+                else {
+                    bank = value & 3;
+                }
+            }
         }
     }
 }
--- Application/ASD.NES.Core/CartridgeComponents/Boards/NROM.cs
+++ Application/ASD.NES.Core/CartridgeComponents/Boards/NROM.cs
@@ -4,7 +4,7 @@
 namespace ASD.NES.Core.CartridgeComponents.Boards {
 
     // https://wiki.nesdev.com/w/index.php/NROM
-    internal abstract class NROM : Board { // TODO: check prg/chr banks count before access by index
+    internal abstract class NROM : Board {
 
         protected override byte Read(int address) {
 
@@ -15,7 +15,10 @@ namespace ASD.NES.Core.CartridgeComponents.Boards {
                 return chr[0][address - 0x6000];
             }
             if (address < 0xC000) {
-                return prg[1][address - 0x8000];
+                if (prg.Count > 1) {
+                    return prg[1][address - 0x8000];
+                }
+                return prg[0][address - 0x8000];
             }
             if (address < 0x10000) {
                 return prg[0][address - 0xC000];
